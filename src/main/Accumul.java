77ba1679b147f0368b653b174937b0c72a09af7b
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-11-05 15:35:45 +0000

    mumbles any string of characters

--- src/main/Accumul.java
+++ src/main/Accumul.java
@@ -1,5 +1,13 @@
+import java.util.stream.IntStream;
+
 public class Accumul {
     public static String accum(String s) {
-        return s.toUpperCase();
+        return String.join("-", IntStream.range(0, s.length())
+                .mapToObj(index -> mumbleCharacter(s.charAt(index), index))
+                .toArray(String[]::new));
+    }
+
+    private static String mumbleCharacter(char c, int index) {
+        return "%s%s".formatted(String.valueOf(c).toUpperCase(), String.valueOf(c).toLowerCase().repeat(index));
     }
 }
