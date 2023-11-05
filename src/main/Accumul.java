1a548264506c6a5077e535ff4ed789b8d473eecc
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-11-05 15:03:07 +0000

    Initial Commit

---
+++ src/main/Accumul.java
@@ -0,0 +1,5 @@
+public class Accumul {
+    public static String accum(String s) {
+        return s;
+    }
+}
---
+++ src/test/AccumulTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AccumulTest {
+
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void accum() {
+        testing(Accumul.accum("A"), "A");
+        testing(Accumul.accum("B"), "B");
+    }
+}
